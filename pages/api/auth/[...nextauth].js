c0654062cd1941304a3115d51901d4ba1490c934
Jordan Vera <verawebdevelopment@gmail.com>
2024-01-15 00:22:01 -0600

    Add PrismaAdapter and update callbacks in NextAuth configuration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,14 @@
 // pages/api/auth/[...nextauth].js
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
@@ -11,6 +16,21 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      // You can perform additional actions here upon sign in
+      console.log('USSER');
+      console.log(user);
+      return true;
+    },
+    async session({ session, user, token }) {
+      // You can add more user data to the session here
+      session.userId = user.id;
+      return session;
+    },
+    // ...other callbacks
+  },
 
   // Additional NextAuth configuration...
 });
