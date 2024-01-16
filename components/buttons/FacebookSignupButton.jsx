6f56a5b5f0725201d9eed0ad1cd588bb8e1b1356
Jordan Vera <verawebdevelopment@gmail.com>
2024-01-16 00:25:32 -0600

    Add Facebook sign-in button with Material Tailwind styling. Facebook oauth fully working.

--- components/buttons/FacebookSignupButton.jsx
+++ components/buttons/FacebookSignupButton.jsx
@@ -1,8 +1,22 @@
 import { signIn } from 'next-auth/react';
+import { Button } from '@material-tailwind/react';
 
 const FacebookSignupButton = () => {
   return (
-    <button onClick={() => signIn('facebook')}>Sign in with Facebook</button>
+    <Button
+      onClick={() => signIn('facebook', { callbackUrl: '/dashboard' })}
+      size="sm"
+      className="bg-gradient-to-b from-blue-700 to-blue-900 group relative flex items-center gap-3 overflow-hidden px-3 py-3 capitalize"
+    >
+      Sign in with Facebook
+      <span className="absolute right-0 grid h-full w-12 place-items-center bg-blue-500 transition-colors ">
+        <img
+          src="/icons/github-mark-white.svg"
+          alt="github icon"
+          className="h-6 w-6"
+        />
+      </span>
+    </Button>
   );
 };
 export default FacebookSignupButton;
